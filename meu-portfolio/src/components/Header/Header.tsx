e0d718c0736cf7257904b9a07c33cf487da11cd7
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-15 01:38:11 -0300

    Home section styling

--- meu-portfolio/src/components/Header/Header.tsx
+++ meu-portfolio/src/components/Header/Header.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './Header.css';
 
 function Header({ id }: { id: string }) {
-    return (
-        <header className="header" id={id}>
-            <h1>Felipe de Moraes</h1>
-            <p>Data Scientist</p>
-        </header>
-    );
+  return (
+    <header id={id} className="header">
+      <h1>Felipe de Moraes</h1>
+      <p>👋 Hello, my name is Felipe de Moraes, and I am a Data Scientist specializing in Python. I am continually seeking to acquire professional knowledge and skills relevant to my career in Data Science. Welcome to my portfolio, and enjoy your visit!</p>
+    </header>
+  );
 }
-
 export default Header;
